a4b2654fdffe6eddbffd3d815e67dd3368f717bb
Alivia (Via) <via.fairchild@gmail.com>
2022-04-20 21:17:22 -0600

    try this

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Alivia Fairchild on 4/20/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
