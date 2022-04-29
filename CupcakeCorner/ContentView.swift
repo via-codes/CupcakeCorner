8038f0e98f172e75ebbef6d9280c379f06263216
Alivia (Via) <via.fairchild@gmail.com>
2022-04-29 16:01:25 -0600

    built out the app more

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,62 +7,42 @@
 
 import SwiftUI
 
-//class User: ObservableObject, Codable {
-//    enum CodingKeys: CodingKey {
-//        case name
-//    }
-//    @Published var name = "Paul"
-//
-//    required init(from decoder: Decoder) throws {
-//        let container = try decoder.container(keyedBy: CodingKeys.self)
-//        name = try container.decode(String.self, forKey: .name)
-//    }
-//    func encode(to encoder: Encoder) throws {
-//        var container = encoder.container(keyedBy: CodingKeys.self)
-//        try container.encode(name, forKey: .name)
-//    }
-//}
-
-struct Response: Codable {
-    var results: [Result]
-}
-
-struct Result: Codable {
-    var trackId: Int
-    var trackName: String
-    var collectionName: String
-}
-
 
 struct ContentView: View {
-    @State private var results = [Result]()
-
-    var body: some View {
-        List(results, id: \.trackId) { item in
-            VStack(alignment: .leading) {
-                Text(item.trackName)
-                    .font(.headline)
-                Text(item.collectionName)
+    @StateObject var order = Order()
+    
+    var body: some View{
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices) {
+                            Text(Order.types[$0])
+                        }
+                    }
+                    
+                    Stepper("Number of cakes: \(order.quantity)", value:
+                                $order.quantity, in: 3...20)
+                }
+                
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled.animation())
+                    
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                
+                Section{
+                    NavigationLink {
+                        AddressView(order: order)
+                    } label: {
+                        Text("Deliver details")
+                    }
+                }
             }
-        }
-        .task {
-            await loadData()
-        }
-    }
-    func loadData() async {
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-    }
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from:
-                                                                data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
+            .navigationTitle("Cupcake Corner")
         }
     }
 }
